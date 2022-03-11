58ebcf5af3cf2eba365b976af14243202c8e588f
Paul Gottinger <pgottinger@outlook.com>
2022-03-11 15:52:01 -0600

    updating notes working

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const PORT = 3001;
+const db = require("./db/db.json");
 
-app.get("/html", (req, res) => {
-  res.sendFile(__dirname + "./public/index.html");
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/notes", (req, res) => {
+  res.json(db);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/api/notes", (req, res) => {
+  db.push(req.body);
+  fs.writeFile("./db/db.json", JSON.stringify(db), () => {
+    console.log(req.body);
+    res.json(req.body);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
